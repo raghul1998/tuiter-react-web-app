bc47aaa655103e0cd3d4225932c1afd5fdef723b
raghul suresh <raghul1998@gmail.com>
2022-09-07 18:01:56 -0400

    app js edit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Yes.
+          Welcome to <h2> TUITER </h2>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/raghul1998/tuiter-react-web-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tuiter SourceCode Here!!!
         </a>
       </header>
     </div>
