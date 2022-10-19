c7b94759a1e86f398db5ce42e0a33e1dd18954a9
raghul suresh <raghul1998@gmail.com>
2022-10-18 21:45:39 -0400

    initial commit labs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Labs from "./labs";
+import HelloWorld from "./labs/a6/hello-world";
+import Tuiter from "./tuiter";
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to <h2> TUITER </h2>
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/raghul1998/tuiter-react-web-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tuiter SourceCode Here!!!
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                    <Route index element={<Labs/>}/>
+                    <Route path="/hello" element={<HelloWorld/>}/>
+                    <Route path="/tuiter" element={<Tuiter/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
